fdd7ec384c7e5073e83f2648349930dcc0c8b687
Cesar Montalvo <cesar_mrc@outlook.com>
2025-06-19 22:08:41 -0500

    Fix Dockerfile and compose.yaml file

--- lawyer.api.clients.webapi/Program.cs
+++ lawyer.api.clients.webapi/Program.cs
@@ -26,11 +26,11 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("all");
 
